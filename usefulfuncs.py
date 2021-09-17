e0b9445ab215f84887142196e598f5cae41ae9c6
MatthewYoung9 <77938756+MatthewYoung9@users.noreply.github.com>
2021-09-17 12:52:54 +0100

    Update usefulfuncs.py
    Included useful functions from time at Pendragon

--- usefulfuncs.py
+++ usefulfuncs.py
@@ -1,4 +1,7 @@
 import numpy as np
+import pyautogui as p
+import time as t
+import PIL
 # handy functions
 
 def find_index(my_list, my_item):
@@ -94,4 +97,72 @@ def rolling_ave(my_list,r):
     for i in range(n):
         rolling[i] = mean( [my_list[i+j] for j in range(r)  ])
     return rolling
+
+def get_pixel_colour(i_x, i_y):
+    # taken from https://rosettacode.org/wiki/Color_of_a_screen_pixel#Python
+	import PIL.ImageGrab
+	return PIL.ImageGrab.grab().load()[i_x, i_y]
+
+def wait_to_load(position,colour):
+    while get_pixel_colour(position[0],position[1])!= colour:
+        n = 0
+    t.sleep(1)
+    
+def auto_sbi(account_code, numbers):
+    BLUE = (51,102,153)
+    WHITE = (255,255,255)
+    for number in numbers:
+        p.click(x=432, y=172)
+        p.write(account_code)  # tpye in acct num
+        p.click(1791,241) # click search
+        wait_to_load((936,674),BLUE)
+        p.press("enter") #click to process SBI doc
+        wait_to_load((1097,767),BLUE)
+        p.write(str(number))
+        p.hotkey('alt', 's') # search for the iunvoice
+        wait_to_load((902,610),WHITE)   
+        p.click(1265,389)
+        p.hotkey('alt', 'a')
+        a = p.position()
+        t.sleep(8)
+        if get_pixel_colour(831,538) == (255, 255, 0):
+            the_index = numbers.index(number)
+            numbers = numbers[the_index+1:]
+            p.press("enter")
+            t.sleep(2)
+            p.hotkey("alt","c")
+            t.sleep(2)
+            p.doubleClick(x=129, y=418)
+            t.sleep(5)
+            auto_sbi(account_code, numbers)
+        wait_to_load((1012,168),WHITE)
+        p.click(1664,417)
+        wait_to_load( (859,694) , (255,255,192) )
+        p.click(1069,587)
+        wait_to_load( (1099,723), BLUE  )
+        p.click(1252,723)
+        wait_to_load( (859,694) , (255,255,192) )
+        t.sleep(1)
+        p.click(1436,909)
+        wait_to_load((959, 700), WHITE)
+        t.sleep(2)
+        p.doubleClick(848,381)
+        wait_to_load((1104, 612),BLUE)
+        wait_to_load((581, 232),BLUE)
+        t.sleep(2)
+        p.doubleClick(x=1089, y=615)
+        t.sleep(2)
+        p.doubleClick(x=1089, y=615)
+        t.sleep(2)
+
+        
+
+def log_on_vpn():
+    p.doubleClick(145,294)
+    wait_to_load((929, 559),(0, 120, 189))
+    p.click(908, 695)
+    p.write("matthew.young") #not my actualy username
+    p.click(968,728)
+    p.write("Uga#390DAN") #not my actual password
+    p.press("enter")
 
